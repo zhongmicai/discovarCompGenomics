3e94cb28925ccabd05fb3636f47e965470cbaa75
nbedelman <nedelman@g.harvard.edu>
2016-06-30 10:29:45 -0400

    a script to run hal2maf with a specific reference scaffold

---
+++ scripts/extractMaf.sh
@@ -0,0 +1,13 @@
+#extractMaf.sh
+#Nate Edelman, 6/29/16
+#Takes a hal file, a reference genome, and a scaffold name.
+#returns a maf file of the alignment
+
+module load hal
+
+halFile=$1
+refGenome=$2
+scafName=$3
+mafFile=$(basename $halFile .hal)_/$scafName/.maf
+
+hal2maf --noAncestors --refGenome $refGenome --refSequence $scafName $halFile $mafFile
